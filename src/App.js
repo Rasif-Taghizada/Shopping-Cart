26f594e9351d9c8e334b644613903fbdbe4fcda5
Rasif <rasiftagizade11@gmail.com>
2022-09-19 17:03:30 +0400

    Shopping Cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext } from 'react';
+import './styles.css';
+import { Routes, Route } from "react-router-dom";
+import Products from "./components/Products";
+import Cart from './components/Cart';
+import { data } from './data';
+
+export const BookContext = createContext()
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BookContext.Provider value={data}>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BookContext.Provider>
   );
 }
 
